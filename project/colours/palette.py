545b9af12ddeec99ef9558c18648e8d25738fe7f
fleeting-mien <fleeting_mien@proton.me>
2022-10-28 12:09:40 +0300

    added a colour palette. pycharm project draws an angry face

---
+++ project/colours/palette.py
@@ -0,0 +1,10 @@
+class palette:
+    salmon = (255, 123, 99)
+    beige = (244, 232, 208)
+    brown = (115, 73, 46)
+    black = (54, 28, 4)
+    light_blue = (176, 220, 245)
+    dark_blue = (94, 59, 176)
+    purple = (139, 68, 152)
+    pink = (202, 71, 196)
+    yellow = (241, 177, 0)
