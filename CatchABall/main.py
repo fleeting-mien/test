0a75d2748ca5dd1e7c66a73fedfe1c5e01f6ccfa
fleeting-mien <fleeting_mien@proton.me>
2022-11-17 18:08:02 +0300

    implemented movement

--- CatchABall/main.py
+++ CatchABall/main.py
@@ -1,44 +1,27 @@
 import pygame
 # from pygame.draw import *
-from random import randint
+# from random import randint
 
 # from colours import *
-from settings import *
+# from settings import *
 from objects import *
 
 pygame.init()
-FPS = 2
 screen = pygame.display.set_mode((MAX_X, MAX_Y))
-
-count = 0
-
-
-def new_ball():
-    x = randint(BORDER, MAX_X - BORDER)
-    y = randint(BORDER, MAX_Y - BORDER)
-    r = randint(DEFAULT_RADIUS - 20, DEFAULT_RADIUS + 20)
-    colour = PALETTE[randint(0, 5)]
-    global current_ball
-    current_ball = Ball(x, y, r, colour)
 
 
 def click(event):
     distance_to_center_of_ball = ((event.pos[0] - current_ball.x) ** 2
                                   + (event.pos[1] - current_ball.y) ** 2) ** 0.5
-    if distance_to_center_of_ball <= BULLSEYE:
-        add = 5
-    elif distance_to_center_of_ball <= current_ball.radius:
-        add = 1
-    else:
-        add = 0
-    new_ball()
+    add = current_ball.value
+    current_ball.randomize()
     return add
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-new_ball()
+current_ball = Ball()
 score = 0
 font = pygame.font.SysFont("Calibri", 18)
 while not finished:
@@ -52,7 +35,7 @@ while not finished:
                 score += add
             else:
                 score = 0
-
+    current_ball.tick()
     current_ball.render(screen)
     label = font.render("SCORE: " + str(score), 1, WHITE)
     screen.blit(label, (50, 50))
--- CatchABall/objects.py
+++ CatchABall/objects.py
@@ -1,5 +1,6 @@
 from settings import *
 from pygame.draw import circle
+from random import randint
 
 
 class Ball:
@@ -8,11 +9,35 @@ class Ball:
     radius = DEFAULT_RADIUS
     colour = DEFAULT_COLOUR
 
-    def __init__(self, x, y, r, colour):
-        self.x = x
-        self.y = y
-        self.r = r
+    def __init__(self, coords=(MAX_X / 2, MAX_Y / 2), radius=DEFAULT_RADIUS,
+                 colour=DEFAULT_COLOUR, speed=(0, 0), value=1):
+        self.x, self.y = coords
+        self.radius = radius
         self.colour = colour
+        self.Vx, self.Vy = speed
+        self.value = value
 
     def render(self, screen):
         circle(screen, self.colour, (self.x, self.y), self.radius)
+
+    Vx = 0
+    Vy = 0
+
+    def tick(self):
+        self.x += self.Vx
+        self.y += self.Vy
+        self.collide()
+
+    def collide(self):
+        if self.x >= (MAX_X - self.radius) or self.x <= self.radius:
+            self.Vx = -self.Vx
+        if self.y >= (MAX_Y - self.radius) or self.y <= self.radius:
+            self.Vy = -self.Vy
+
+    def randomize(self):
+        self.x = randint(BORDER, MAX_X - BORDER)
+        self.y = randint(BORDER, MAX_Y - BORDER)
+        self.radius = randint(DEFAULT_RADIUS - 20, DEFAULT_RADIUS + 20)
+        self.colour = PALETTE[randint(0, NUM_COLOURS - 1)]
+        self.Vx = randint(-MAX_SPEED, MAX_SPEED)
+        self.Vy = randint(-MAX_SPEED, MAX_SPEED)
