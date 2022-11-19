4ec01b18da20d8f3db6e69a54cb1b37634cdd3d1
fleeting-mien <fleeting_mien@proton.me>
2022-11-19 13:32:47 +0300

    value based on speed + speed doesn't change with fps

--- CatchABall/objects.py
+++ CatchABall/objects.py
@@ -1,23 +1,28 @@
 from settings import *
 from pygame.draw import circle
-from random import randint
+from random import randint, uniform
 
 
 class Ball:
     def __init__(self, coords=CENTER, radius=AVERAGE_RADIUS,
-                 colour=DEFAULT_COLOUR, speed=(0, 0), value=0):
+                 colour=DEFAULT_COLOUR, speed=(0, 0)):
         self.x, self.y = coords
         self.radius = radius
         self.colour = colour
-        self.Vx, self.Vy = speed
-        self.value = value
+        self.Vx, self.Vy = speed    # пикселей в секунду
+
+        v = (speed[0] ** 2 + speed[1] ** 2) ** 0.5
+        step = MAX_V / 5
+        self.value = int(v / step) + 1
+        if self.value > 5:
+            self.value = 5
 
     def render(self, screen):
         circle(screen, self.colour, (self.x, self.y), self.radius)
 
     def tick(self):
-        self.x += self.Vx
-        self.y += self.Vy
+        self.x += self.Vx / FPS
+        self.y += self.Vy / FPS
         self.collide()
 
     def collide(self):
@@ -31,6 +36,6 @@ class Ball:
         self.y = randint(BORDER, MAX_Y - BORDER)
         self.radius = randint(MIN_RADIUS, MAX_RADIUS)
         self.colour = PALETTE[randint(0, NUM_COLOURS - 1)]
-        self.Vx = randint(-MAX_SPEED, MAX_SPEED)
-        self.Vy = randint(-MAX_SPEED, MAX_SPEED)
+        self.Vx = uniform(-MAX_SPEED, MAX_SPEED)
+        self.Vy = uniform(-MAX_SPEED, MAX_SPEED)
         self.value = 1
